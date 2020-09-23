c192e43e82af742d1c7c78cd27e25fc9a19a476e
Steven Vitale <steve@bioinf.us>
2020-09-23 17:03:07 -0400

    add mkpda.sh

---
+++ build/mkpda.sh
@@ -0,0 +1,18 @@
+#start from scratch
+mv pda deleteme_pda
+rm -fR pda-cran
+#rsync and exclude directories that CRAN doesn't like
+mkdir pda-cran
+rsync -av ../R ./pda-cran/
+rsync -av ../man ./pda-cran/
+rsync -av ../src ./pda-cran/ --exclude=*.sw* --exclude=*.so --exclude=*.o --exclude=*.rds 
+cp ../DESCRIPTION ./pda-cran/
+cp ../NAMESPACE  ./pda-cran
+#create tehe source package
+R CMD build pda-cran
+#extract
+tar -xzvf pda_1.0.tar.gz
+#check the package
+R CMD check --as-cran pda
+#build the binary
+R CMD INSTALL --build pda_1.0.tar.gz
