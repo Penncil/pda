a25698ed28e0c51309bae7c6cddfc2195ffd0b3f
Steven Vitale <steve@bioinf.us>
2020-09-23 17:25:04 -0400

    build/mkpda.sh

--- build/mkpda.sh
+++ build/mkpda.sh
@@ -1,3 +1,4 @@
+#run from build!
 #start from scratch
 mv pda deleteme_pda
 rm -fR pda-cran
@@ -8,7 +9,7 @@ rsync -av ../man ./pda-cran/
 rsync -av ../src ./pda-cran/ --exclude=*.sw* --exclude=*.so --exclude=*.o --exclude=*.rds 
 cp ../DESCRIPTION ./pda-cran/
 cp ../NAMESPACE  ./pda-cran
-#create tehe source package
+#create the source package
 R CMD build pda-cran
 #extract
 tar -xzvf pda_1.0.tar.gz
