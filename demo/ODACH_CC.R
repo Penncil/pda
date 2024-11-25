a7a0cf043da82dcf78bfe96dc885a890582b1e52
chongliang-luo <luocl3009@mail.com>
2024-11-25 12:56:28 -0500

    fix categorical var name/level bug

--- demo/ODACH_CC.R
+++ demo/ODACH_CC.R
@@ -5,6 +5,12 @@ require(data.table)
 # load('/Users/chongliangluo/Library/CloudStorage/Dropbox/PDA-git/pda/data/odach_cc.rda')
 # setwd('/Users/chongliangluo/Library/CloudStorage/Dropbox/PDA_test/CL/')
 
+# n_rows <- nrow(odach_cc)
+# odach_cc$Category <- factor(rep(c("X (X,Y,Z)", "Y (X,Y,Z)", "Z (X,Y,Z)"), length.out = n_rows))
+# names(odach_cc)[8] = 'Group (A,B,C)'
+# save(odach_cc, file='/Users/chongliangluo/Library/CloudStorage/Dropbox/PDA-git/pda/data/odach_cc.rda')
+
+
 ## In the toy example below we aim to analyze the association of survival {time} 
 ##  with X1, Category, and Group using Cox reg with case-cohort design,
 ## data(odach_cc) simulated, subsampled case+cohort data, 3 sites: 'site1', 'site2', 'site3'
@@ -19,7 +25,7 @@ data(odach_cc)
 data_split <- split(odach_cc, odach_cc$site)
 
 ## cch stratified by site, using self-written cch_pooled()
-fit.pool <- cch_pooled(Surv(time, status) ~ X1+Group+Category, data=odach_cc,  
+fit.pool <- cch_pooled(Surv(time, status) ~ X1+`Group (A,B,C)`+Category, data=odach_cc,  
                        full_cohort_size=c(site1=800,site2=600,site3=400), method='Prentice')
 fit.pool$par
 #          X1      GroupB      GroupC   CategoryY   CategoryZ 
@@ -27,7 +33,7 @@ fit.pool$par
  
 
 # cch each site
-sapply(1:3, function(i) cch(Surv(time, status) ~ X1 +Group+Category, 
+sapply(1:3, function(i) cch(Surv(time, status) ~ X1+`Group (A,B,C)`+Category, 
                             data = cbind(ID=1:nrow(data_split[[i]]), data_split[[i]]),
                             subcoh = ~subcohort, id = ~ID, 
                             cohort.size =c(site1=800,site2=600,site3=400)[i], 
@@ -43,9 +49,10 @@ control <- list(project_name = 'ODACH case-cohort toy example',
                 model = 'ODACH_CC',
                 family = 'cox',
                 outcome = "Surv(time, status)",
-                variables = c('X1','Group', 'Category'),
+                variables = c('X1','`Group (A,B,C)`', 'Category'),
                 #levels of all categorical X's, with the first being the reference
-                variables_lev = list(Group=c('A','B','C'), Category=c('X','Y','Z')),  
+                variables_lev = list(`Group (A,B,C)`=c('A','B','C'), 
+                                     Category=c('X (X,Y,Z)','Y (X,Y,Z)','Z (X,Y,Z)')),  
                 full_cohort_size = c(site1=800,site2=600,site3=400), # for ODACH_CC
                 method = 'Prentice',                          # for ODACH_CC weights
                 optim_method = 'BFGS',
@@ -68,7 +75,6 @@ pda(site_id = 'site1', control = control, dir = mydir, upload_without_confirm = 
 S=readline(prompt="Type  <Return>   to continue : ")
 ## assume remote site3: enter "1" to allow tranferring your local estimate
 pda(site_id = 'site3', ipdata = data_split[[3]], dir=mydir, upload_without_confirm = T)
- 
   
 S=readline(prompt="Type  <Return>   to continue : ")
 ## assume remote site2: enter "1" to allow tranferring your local estimate
