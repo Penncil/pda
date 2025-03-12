837d033dc72f6782fefbbc9731d1d357e1e8671f
Chongliang Luo <luocl3009@gmail.com>
2025-03-11 23:41:40 -0500

    ODACH_CC improve robust var est
     In demo(ODACH_CC): run one more round of ODACH_CC use the obtained ODACH_CC point est as initial

--- demo/ODACH_CC.R
+++ demo/ODACH_CC.R
@@ -6,12 +6,6 @@ require(spatstat) # for using weighted.median() as init
 # load('/Users/chongliangluo/Dropbox/PDA-git/pda/data/odach_cc.rda')
 # setwd('/Users/chongliangluo/Dropbox/PDA_test/CL/')
 
-# n_rows <- nrow(odach_cc)
-# odach_cc$Category <- factor(rep(c("X (X,Y,Z)", "Y (X,Y,Z)", "Z (X,Y,Z)"), length.out = n_rows))
-# names(odach_cc)[8] = 'Group (A,B,C)'
-# save(odach_cc, file='/Users/chongliangluo/Library/CloudStorage/Dropbox/PDA-git/pda/data/odach_cc.rda')
-
-
 ## In the toy example below we aim to analyze the association of survival {time} 
 ##  with X1, Category, and Group using Cox reg with case-cohort design,
 ## data(odach_cc) simulated, subsampled case+cohort data, 3 sites: 'site1', 'site2', 'site3'
@@ -22,7 +16,7 @@ require(spatstat) # for using weighted.median() as init
 ## will be assigned to the sites at the server https://pda.one.
 ## Each site can access via web browser to check the communication of the summary stats.
 
-data(odach_cc)
+# data(odach_cc)
 odach_cc$Category = as.character(odach_cc$Category)
 # to test site3 has missing category Z for "Category" variable
 odach_cc[odach_cc$site=='site3'&odach_cc$Category=='Z (X,Y,Z)', ]$Category='Y (X,Y,Z)'
@@ -58,86 +52,74 @@ fit.pool$par
 sqrt(diag(-solve(fit.pool$hessian)))  # naive s.e. from inv hessian 
 # 0.1105210         0.3184666         0.2969597         0.2831813         0.3211316 
 sqrt(diag(fit.pool$var)) # robust s.e. from sandwich 
-# 0.1358760         0.3567799         0.3109574         0.3122135         0.3692968 
-# 0.1150233         0.3576904         0.3655076         0.3193014         0.3499519  # updated...
-
-# precision <- min(diff(sort(odach_cc$time)))/2 # 1e-4
-# odach_cc$time_in = 0
-# odach_cc[odach_cc$subcohort == 0, "time_in"] <- odach_cc[odach_cc$subcohort == 0, "time"] - precision
-# odach_cc$ID = 1:nrow(odach_cc)
-# fit.pool <- coxph(Surv(time_in, time, status) ~ X1+`Group (A,B,C)`+Category + strata(site)+cluster(ID), data=odach_cc, robust=T)
-# sqrt(diag(fit.pool$var))
-#  0.1153119 0.3363288 0.3260168 0.3059612 0.3590618
+# 0.1150233         0.3576904         0.3655076         0.3193014         0.3499519   
 
 
-# cch each site
-sapply(1:3, function(i) cch(Surv(time, status) ~ X1+`Group (A,B,C)`+Category, 
-                            data = cbind(ID=1:nrow(data_split[[i]]), data_split[[i]]),
-                            subcoh = ~subcohort, id = ~ID, 
-                            cohort.size =c(site1=800,site2=600,site3=400)[i], 
-                            method = 'Prentice')$coef) 
-#        XX1  X`Group (A,B,C)`B  X`Group (A,B,C)`C XCategoryY (X,Y,Z) XCategoryZ (X,Y,Z) 
-# -0.5184543          0.4805595          0.8929081         -0.5006887         -0.1715390 
-# -0.4707972          0.2383644          0.6133549         -0.7252045          0.1941511 
-# -0.5406719         -0.1171266         -1.2518623          0.3119451 
-
-
-# S=readline(prompt="Type  <Return>   to continue : ")
 # ############################  STEP 1: initialize  ############################### 
 control$init_method = 'meta' # 'lead' # 'weighted.median' #  'median'
 
 ## run the example in local directory:
 ## specify your working directory, default is the tempdir
 mydir <- getwd()   # tempdir()
-## assume lead site1: enter "1" to allow transferring the control file
 pda(site_id = 'site1', control = control, dir = mydir, upload_without_confirm = T,silent_message =F)
 
-# S=readline(prompt="Type  <Return>   to continue : ")
-## assume remote site3: enter "1" to allow tranferring your local estimate
 pda(site_id = 'site3', ipdata = data_split[[3]], dir=mydir, upload_without_confirm = T)
   
-# S=readline(prompt="Type  <Return>   to continue : ")
-## assume remote site2: enter "1" to allow tranferring your local estimate
 pda(site_id = 'site2', ipdata = data_split[[2]], dir=mydir, upload_without_confirm = T)
 
-# S=readline(prompt="Type  <Return>   to continue : ")
-## assume lead site1: enter "1" to allow tranferring your local estimate
 ## control.json is also automatically updated
 pda(site_id = 'site1', ipdata = data_split[[1]], dir=mydir, upload_without_confirm = T)
 
-# S=readline(prompt="Type  <Return>   to continue : ")
 # ###################   STEP 2: derivative  #################
-## assume remote site3: enter "1" to allow tranferring your derivatives
 pda(site_id = 'site3', ipdata = data_split[[3]], dir=mydir,upload_without_confirm=T)
 
-# S=readline(prompt="Type  <Return>   to continue : ")
-## assume remote site2: enter "1" to allow tranferring your derivatives
 pda(site_id = 'site2', ipdata = data_split[[2]], dir=mydir,upload_without_confirm=T)
 
-# S=readline(prompt="Type  <Return>   to continue : ")
-## assume lead site1: enter "1" to allow tranferring your derivatives
 pda(site_id = 'site1', ipdata = data_split[[1]], dir=mydir,upload_without_confirm=T)
 
-# S=readline(prompt="Type  <Return>   to continue : ")
 # ############################  STEP 3: estimate  ###############################
-## assume lead site1: enter "1" to allow tranferring the surrogate estimate
+pda(site_id = 'site1', ipdata = data_split[[1]], dir=mydir,upload_without_confirm=T)
+# "beta_init":[-0.51927745,0.17385216,0.25506044,-0.25316367,-0.04065175] # (meta)
+# "btilde":   [-0.51303977,0.12780133,0.24209696,-0.23769306,0.137274],
+# "setilde":[0.11504608,0.35832839,0.36592225,0.31974574,0.34950701]
+
+
+############ 20250311:  to get better robust s.e. estimate ###################### 
+# run one more round of ODACH_CC use the obtained ODACH_CC point est as initial
+# reset control step and beta_init
+config <- getCloudConfig(site_id =control$lead_site, dir=mydir)
+fit.pda = pdaGet(name = 'site1_estimate', config = config)
+control = pdaGet(name = 'control', config = config)
+control$step = 'derive'
+control$beta_init = fit.pda$btilde
+pdaPut(control,'control', config = config, upload_without_confirm = T, silent_message = F)
+
+# ###################   STEP 4: derivative again #################
+
+pda(site_id = 'site3', ipdata = data_split[[3]], dir=mydir,upload_without_confirm=T)
+
+pda(site_id = 'site2', ipdata = data_split[[2]], dir=mydir,upload_without_confirm=T)
+
 pda(site_id = 'site1', ipdata = data_split[[1]], dir=mydir,upload_without_confirm=T)
 
+# ############################  STEP 5: estimate again  ###############################
+pda(site_id = 'site1', ipdata = data_split[[1]], dir=mydir,upload_without_confirm=T)
+# "btilde":[-0.51313047,0.12776128,0.24215611,-0.23726386,0.13630634],
+# "setilde":[0.11502241,0.35768382,0.3655027,0.31930003,0.3499533]
+
+fit.pda = pdaGet(name = 'site1_estimate', config = config)
+
+ 
 ## all coef est:
 # pooled:                     [-0.5129684,  0.1277404, 0.2421812, -0.2374705, 0.1359541
 # meta:                       [-0.51927745,0.17385216,0.25506044,-0.25316367,-0.04065175]
-# median:                     [-0.51845429, 0.23836445,0.61335488,-0.50068871,0]
-# weighted.median:            [-0.51845429, 0.23836445,0.61335488,-0.50068871,0]
-# lead:                       [-0.51845429, 0.48055952,0.89290808,-0.50068871,-0.17153903]
-# odach_cc(meta):             [-0.51303977,0.12780133,0.24209696,-0.23769306,0.137274]
-# odach_cc(median):           [-0.51369825, 0.11965385,0.24015613,-0.23640535,0.13420135]
-# odach_cc(weighted.median):  [-0.51369825, 0.11965385,0.24015613,-0.23640535,0.13420135]
-# odach_cc(lead):             [-0.5158358,  0.08234449,0.21134127,-0.24025793,0.13681052]
+# odach_cc(meta):             [-0.51303977,0.12780133,0.24209696,-0.23769306,0.137274]   # first round 
+# odach_cc(again):            [-0.51313047,0.12776128,0.24215611,-0.23726386,0.13630634] # second round 
 
-## all var est:
-# "setilde": [0.11504608,0.35832839,0.36592225,0.31974574,0.34950701]
+## all s.e. est: 
 # cch_pooled  0.1150233, 0.3576904, 0.3655076, 0.3193014, 0.3499519 
-
+# "setilde": [0.11504608,0.35832839,0.36592225,0.31974574,0.34950701]  # first round
+# "setilde": [0.11502241,0.35768382,0.3655027,0.31930003,0.3499533]    # second round
 # btilde and setilde (robust) are almost identical to pooled cch! 
 
 
@@ -150,3 +132,35 @@ pda(site_id = 'site1', ipdata = data_split[[1]], dir=mydir,upload_without_confir
 ## the PDA ODACH_CC is now completed!
 ## All the sites can still run their own surrogate estimates and broadcast them.
  
+
+
+
+
+
+
+
+
+
+
+# ## coxph trick
+# precision <- min(diff(sort(odach_cc$time)))/2 # 1e-4
+# odach_cc$time_in = 0
+# odach_cc[odach_cc$subcohort == 0, "time_in"] <- odach_cc[odach_cc$subcohort == 0, "time"] - precision
+# odach_cc$ID = 1:nrow(odach_cc)
+# fit.pool1 <- coxph(Surv(time_in, time, status) ~ X1+`Group (A,B,C)`+Category + strata(site)+cluster(ID), data=odach_cc, robust=T)
+# fit.pool1$coef
+# # -0.5129683         0.1277399         0.2421812        -0.2374701         0.1359542 
+# sqrt(diag(fit.pool1$var))
+# # 0.1153119 0.3363288 0.3260168 0.3059612 0.3590618
+
+
+# cch each site
+sapply(1:3, function(i) cch(Surv(time, status) ~ X1+`Group (A,B,C)`+Category, 
+                            data = cbind(ID=1:nrow(data_split[[i]]), data_split[[i]]),
+                            subcoh = ~subcohort, id = ~ID, 
+                            cohort.size =c(site1=800,site2=600,site3=400)[i], 
+                            method = 'Prentice')$coef) 
+#        XX1  X`Group (A,B,C)`B  X`Group (A,B,C)`C XCategoryY (X,Y,Z) XCategoryZ (X,Y,Z) 
+# -0.5184543          0.4805595          0.8929081         -0.5006887         -0.1715390 
+# -0.4707972          0.2383644          0.6133549         -0.7252045          0.1941511 
+# -0.5406719         -0.1171266         -1.2518623          0.3119451 
